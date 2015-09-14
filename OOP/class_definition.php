cb9fdd2a6c4c28f649e080a067ff459b639cbc71
depeenderrathore <deepenrathore28@gmail.com>
2015-09-15 00:54:39 +0530

    OOP:get_class_methods in PHP

--- OOP/class_definition.php
+++ OOP/class_definition.php
@@ -1,12 +1,14 @@
 <?php 
 
 class Cars{
-
+	function gretting(){
+		echo 'Deepak';
+	}
 }
 
-$my_classes = get_declared_classes();
+$my_methods = get_class_methods('Cars');
 
-foreach ($my_classes as $class) {
-	echo $class . " ";
+foreach ($my_methods as $fun) {
+	echo $fun;
 }
 ?>
