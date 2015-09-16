ae8053929c326451517a5540a02b351f27fc526c
depeenderrathore <deepenrathore28@gmail.com>
2015-09-16 19:00:27 +0530

    Inheritance

--- OOP/class_definition.php
+++ OOP/class_definition.php
@@ -1,17 +1,21 @@
 <?php 
 
 class Cars{
-
-	var $wheel_count = 4;
-	var $door_count = 4;
-
+	var $wheels = 4;
 	function car_details(){
-		return "This car has " . $this->wheel_count . 'wheels.';
+		return 'Hello';
 	}
 
 }
 
 $bmw = new Cars();
 
-echo $bmw->car_details();
+class Trucks extends Cars{
+
+}
+
+$tacoma = new Trucks();
+
+echo $tacoma->wheels;
+
 ?>
