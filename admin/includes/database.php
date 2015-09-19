d1a647f262a0982c5c2227e1ade73b0089c4181a
depeenderrathore <deepenrathore28@gmail.com>
2015-09-19 11:35:16 +0530

    The Query method

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -11,12 +11,22 @@ class Database{
 
 	}
 	public function open_db_connection(){
-		
+
 		$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 		
 		if(mysqli_connect_errno()){
 			die("Database connection failed." . mysqli_error());
 		}
+	}
+
+	public function query($sql){
+		$result = mysqli_query($this->connection,$sql);
+
+		if(!$result){
+			die("Query Failed!").
+		}
+
+		return $result;
 	}
 	
 }
