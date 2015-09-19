2288485b8c70811149774966f8b1db26fb2a380b
depeenderrathore <deepenrathore28@gmail.com>
2015-09-19 11:32:08 +0530

    Automatically DB connection setup

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,6 +8,13 @@
           Admin
           <small>Subheading</small>
         </h1>
+
+
+  
+
+
+
+
         <ol class="breadcrumb">
           <li>
             <i class="fa fa-dashboard"></i><a href="index.html">Dashboard</a>
--- admin/includes/database.php
+++ admin/includes/database.php
@@ -5,7 +5,13 @@ class Database{
 
 	private $connection;
 
+	function __construct(){
+
+		$this->open_db_connection();
+
+	}
 	public function open_db_connection(){
+		
 		$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 		
 		if(mysqli_connect_errno()){
@@ -15,6 +21,5 @@ class Database{
 	
 }
 
-$database = new Database();
-$database->open_db_connection();
+
 
